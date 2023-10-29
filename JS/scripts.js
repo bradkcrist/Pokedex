df6dd56d9c87b82ae3f27b135eae16f1655f416e
bradkcrist <bradkcrist08@gmail.com>
2023-10-29 05:34:36 -0600

    Added pokemon list to scripts.js

---
+++ JS/scripts.js
@@ -0,0 +1,3 @@
+let pokemonList = [{name:"Regirock" , type:"Rock", height:"1.7"}];
+ pokemonList = [{name:"Thundrus" , type:["Electric","Flying"] , height:"1.5"}];
+ pokemonList = [{name:"Zekrom" , type:["Dragon","Electric"] , height:"2.9"}];
