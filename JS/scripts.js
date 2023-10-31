bd0fe81f615c07937ee61a2bf9f8cc5d9db18776
bradkcrist <bradkcrist08@gmail.com>
2023-10-30 20:50:53 -0600

    changed my original into an actual array on scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,8 @@
-let pokemonList = [{name:"Regirock" , type:"Rock", height:"1.7"}];
- pokemonList = [{name:"Thundrus" , type:["Electric","Flying"] , height:"1.5"}];
- pokemonList = [{name:"Zekrom" , type:["Dragon","Electric"] , height:"2.9"}];
+// let pokemonList = [{name:"Regirock" , type:"Rock", height:"1.7"}];
+// pokemonList = [{name:"Thundrus" , type:["Electric","Flying"] , height:"1.5"}];
+//  pokemonList = [{name:"Zekrom" , type:["Dragon","Electric"] , height:"2.9"}];
+let pokemonList = [{ name: "Regirock", type: "Rock", height: "1.7" }, { name: "Thundrus", type: ["Electric", "Flying"], height: "1.5" }, { name: "Zekrom", type: ["Dragon", "Electric"], height: "2.9" }];
+for (let i = 0; i < 3; i++) {
+    document.write(pokemonList[i].name); 
+    
+}
