cc0829f56c1eb6f6996cb5093be439277b06b3c0
bradkcrist <bradkcrist08@gmail.com>
2023-10-31 04:18:47 -0600

    Escaped double quotes for single quotes in scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,8 +1,7 @@
 // let pokemonList = [{name:"Regirock" , type:"Rock", height:"1.7"}];
 // pokemonList = [{name:"Thundrus" , type:["Electric","Flying"] , height:"1.5"}];
 //  pokemonList = [{name:"Zekrom" , type:["Dragon","Electric"] , height:"2.9"}];
-let pokemonList = [{ name: "Regirock", type: "Rock", height: "1.7" }, { name: "Thundrus", type: ["Electric", "Flying"], height: "1.5" }, { name: "Zekrom", type: ["Dragon", "Electric"], height: "2.9" }];
+let pokemonList = [{ name: 'Regirock', height: '1.7', type: 'Rock'}, { name: 'Thundrus',height: '1.5', type: ['Electric', 'Flying']}, { name: 'Zekrom', height: '2.9', type: ['Dragon', 'Electric']}];
 for (let i = 0; i < 3; i++) {
-    document.write(pokemonList[i].name); 
-    
+    document.write(pokemonList[i].name);     
 }
