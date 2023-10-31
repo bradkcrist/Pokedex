37c183af5efb9b606d220275fc0b084dcbd5cd27
bradkcrist <bradkcrist08@gmail.com>
2023-10-31 05:39:53 -0600

    Added comments to my script.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,20 +1,25 @@
 // /name:"Regirock" , type:"Rock", height:"1.7"}];
 // [{name:"Thundrus" , type:["Electric","Flying"] , height:"1.5"}];
 // [{name:"Zekrom" , type:["Dragon","Electric"] , height:"2.9"}];
+// Pokdex Index name, height, type
 let pokemonList = [
     {
-        name: 'Regirock' + ('(height 1.7)'),
+        name: 'Regirock' + ('(height: 1.7)'),
         type: 'Rock'
     },
     {
-        name: 'Thundrus' + ('(height 1.5)'),
+        name: 'Thundrus' + ('(height: 1.5)'),
          type: ['Electric', 'Flying']
     },
     {
-        name: 'Zekrom' + ('(height 2.9)'),
+        name: 'Zekrom' + ('(height: 2.9)'),
         type: ['Dragon', 'Electric']
     }
 ];
+// Adding loop for pokemon name and height greater then 2.5
 for (let i = 0; i < 3; i++) {
-    document.write(pokemonList[i].name); 
-}
+    document.write(pokemonList[i].name);
+    if (pokemonList.name.height < 2.5 + 'Wow thats big')
+    
+    }
+
