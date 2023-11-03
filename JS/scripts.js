cf108df841d1606f12682fa05ffec14d60524666
bradkcrist <bradkcrist08@gmail.com>
2023-11-03 04:07:25 -0600

    Tried rearranging my objects in my string

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,22 +4,34 @@
 // Pokdex Index name, height, type
 let pokemonList = [
     {
-        name: 'Regirock' + ('(height: 1.7)'),
+        name: "Regirock",
+        height: 1.7,
         type: 'Rock'
     },
     {
-        name: 'Thundrus' + ('(height: 1.5)'),
-         type: ['Electric', 'Flying']
+        name: 'Thundrus',
+        height: 1.5,
+        type: ['Electric', 'Flying']
+
     },
     {
-        name: 'Zekrom' + ('(height: 2.9)'),
+        name: 'Zekrom',
+        height: 2.9,
         type: ['Dragon', 'Electric']
     }
 ];
 // Adding loop for pokemon name and height greater then 2.5
-for (let i = 0; i < 3; i++) {
-    document.write(pokemonList[i].name);
-    if (pokemonList.name.height < 2.5 + 'Wow thats big')
-    
-    }
+ for (let i = 0; i < 3; i++) {
+     document.write(pokemonList[i].name);
+}
+
+// for (let i=0; i < pokemonList.length; i++){
+//     if (pokemonList[i].height < 2.5 && pokemonList[i].height > 2.0){
+//       document.write(pokemonList[i].name + " is a big pokemon");
+//     }else if (pokemonlist[i].age < 1.5){
+//       document.write(pokemonList[i].name + " is a medium pokemon");
+//   }
+// }
+
+
 
