424f01f620a3a2f3c0835a44fbb77e095692d94a
bradkcrist <bradkcrist08@gmail.com>
2023-11-07 03:54:45 -0700

    Added a function to my Pokemon and created a forEach() loop. committed out my original loop

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,6 +2,10 @@
 // [{name:"Thundrus" , type:["Electric","Flying"] , height:"1.5"}];
 // [{name:"Zekrom" , type:["Dragon","Electric"] , height:"2.9"}];
 // Pokdex Index name, height, type
+function pokemon(pokemonList) {
+    return pokemonList.name + pokemonList.height
+}
+
 let pokemonList = [
     {
         name: "Regirock",
@@ -20,13 +24,20 @@ let pokemonList = [
         type: ['Dragon', 'Electric']
     }
 ];
-// Adding FOR loop pokemon name and height greater then 2.5
+
+
+pokemonList.forEach(pokemonList => {
+ document.write (pokemonList.name + '(height:) ' + pokemonList.height + '<br>');
+});
+
+
+// Adding forEach() loop pokemon name and height greater then 2.5
 // Displaying else if for any other pokemon with height lower then 2.0
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 2.5)
-        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + "Wow thats a big pokemon" +  '<br>')
- else if (pokemonList[i].height < 2.0) {
-    document.write(pokemonList[i].name + '(height' + pokemonList[i].height + ') ' + '<br>')
-}
-}
+// for (let i = 0; i < pokemonList.length; i++) {
+    // if (pokemonList[i].height > 2.5)
+        // document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + "Wow thats a big pokemon" +  '<br>')
+//  else if (pokemonList[i].height < 2.0) {
+    // document.write(pokemonList[i].name + '(height' + pokemonList[i].height + ') ' + '<br>')
+// }
+// }
