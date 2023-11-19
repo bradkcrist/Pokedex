072ca0cfc3a96430902547b8c378f68f748b8506
bradkcrist <bradkcrist08@gmail.com>
2023-11-19 05:39:15 -0700

    Created a function for my modal to show the specs of my pokemon

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,9 +4,49 @@
 let pokemonRepository = (function () {
     let pokemonList = [];
     let apiUrl= 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-   
+    let modalContainer = document.querySelector('#modal-container');
 
 // Functions and Prameters
+
+function showModal(title, text, img){
+    modalContainer.innerHTML = '';
+    
+    let modal= document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement= document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText= 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+}
+function hideModal(){
+    modalContainer.classList.remove('is-visible');
+}
+
+window.addEventListener('keydown', (e) => {
+    let target = e.target;
+    if (target === modalContainer){
+        hideModal();
+    }
+});
+
+document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('Pokemon', 'Pokemon Specs');
+});
+})();
 
 function getAll() {
     return pokemonList;
@@ -50,7 +90,6 @@ function loadDetails(item) {
     return fetch(url).then(function (response) {
       return response.json();
     }).then(function (details) {
-      // Now we add the details to the item
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
       item.types = details.types;
@@ -59,9 +98,8 @@ function loadDetails(item) {
     });
   }
 
-
 function showDetails(pokemon) {
-    loadDetails(pokemon).then(function () {
+   loadDetails(pokemon).then(function () {
         console.log(pokemon);
       });
     }
