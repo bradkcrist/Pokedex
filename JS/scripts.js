e1ef6961c0ca1e182ea63ec8de79e3c946f50293
bradkcrist <bradkcrist08@gmail.com>
2023-11-20 05:08:29 -0700

    Moved showModal to the bottom of my function list and chagned my showDetails from console. log(Pokemon) to showModal(pokemon)

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,50 +4,9 @@
 let pokemonRepository = (function () {
     let pokemonList = [];
     let apiUrl= 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-    let modalContainer = document.querySelector('#modal-container');
+    let modalContainer = document.querySelector('#modal-container')
 
 // Functions and Prameters
-
-function showModal(title, text, img){
-    modalContainer.innerHTML = '';
-    
-    let modal= document.createElement('div');
-    modal.classList.add('modal');
-
-    let closeButtonElement= document.createElement('button');
-    closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText= 'Close';
-    closeButtonElement.addEventListener('click', hideModal);
-
-    let titleElement = document.createElement('h1');
-    titleElement.innerText = title;
-
-    let contentElement = document.createElement('p');
-    contentElement.innerText = text;
-
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(contentElement);
-    modalContainer.appendChild(modal);
-
-    modalContainer.classList.add('is-visible');
-}
-function hideModal(){
-    modalContainer.classList.remove('is-visible');
-}
-
-window.addEventListener('keydown', (e) => {
-    let target = e.target;
-    if (target === modalContainer){
-        hideModal();
-    }
-});
-
-document.querySelector('#show-modal').addEventListener('click', () => {
-    showModal('Pokemon', 'Pokemon Specs');
-});
-})();
-
 function getAll() {
     return pokemonList;
 }
@@ -100,9 +59,51 @@ function loadDetails(item) {
 
 function showDetails(pokemon) {
    loadDetails(pokemon).then(function () {
-        console.log(pokemon);
+        showModal(pokemon);
       });
     }
+
+    function showModal(title,text){
+        modalContainer.innerHTML = '';
+        
+        let modal= document.createElement('div');
+        modal.classList.add('modal');
+    
+        let closeButtonElement= document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText= 'Close';
+        closeButtonElement.addEventListener('click', hideModal);
+    
+        let titleElement = document.createElement('h1');
+        titleElement.innerText = title;
+    
+        let contentElement = document.createElement('p');
+        contentElement.innerText = text;
+    
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        modalContainer.appendChild(modal);
+    
+        modalContainer.classList.add('is-visible');
+    }
+    function hideModal(){
+        modalContainer.classList.remove('is-visible');
+    }
+    
+    window.addEventListener('keydown', (e) => {
+        let modalContainer = document.querySelector('#modal-container');
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')){
+            hideModal();
+        }
+    });
+    
+    modalContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target === modalContainer) {
+            hideModal();
+        }
+    });
 
 return {
     getAll: getAll,
